8a6183179a40d2dc69044a2c769004b0304df96c
mlhuynh <greenlinh@gmail.com>
2023-06-19 01:17:51 -0700

    Implemented messageTest to test render one chat message entry in App component.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
 import ChatEntry from './components/ChatEntry';
-import { useState } from 'react';
+// import { useState } from 'react';
 
 const App = () => {
-  const [chatData, setChatData] = useState(chatMessages)
+//   const [chatData, setChatData] = useState(chatMessages)
 
+  const messageTest = chatMessages[0]
+  
   return (
     <div id="App">
       <header>
@@ -14,7 +16,12 @@ const App = () => {
       </header>
       <main>
         <ChatEntry>
-          entries={chatData}
+          {/* entries={chatData} */}
+          id={messageTest.id}
+          sender={messageTest.sender}
+          body={messageTest.body}
+          timeStamp={messageTest.timeStamp}
+          liked={messageTest.liked}
         </ChatEntry>
         {/* Wave 02: Render ChatLog component */}
       </main>
