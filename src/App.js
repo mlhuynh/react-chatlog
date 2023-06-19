b222a111081e96b1cf8db4b42e0478a1aaabc407
mlhuynh <greenlinh@gmail.com>
2023-06-18 23:50:26 -0700

    Updated header application title in App component.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Ada Chat Log</h1>
       </header>
       <main>
         <ChatEntry />
