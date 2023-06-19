de1dca254f11428236684a2a47ca590968bb26c4
mlhuynh <greenlinh@gmail.com>
2023-06-18 23:34:53 -0700

    Added timeStamp prop and PropType in ChatEntry component.

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -9,7 +9,7 @@ const ChatEntry = (props) => {
       <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
         <p>{props.body}</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p className="entry-time">{<TimeStamp time={props.timeStamp} />}</p>
         <button className="like">🤍</button>
       </section>
     </div>
@@ -19,6 +19,7 @@ const ChatEntry = (props) => {
 ChatEntry.propTypes = {
   sender: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
+  timeStamp: PropTypes.string.isRequired
 };
 
 export default ChatEntry;
