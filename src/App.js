de48f8fa615b4624a4d5bfc89081a74d918627ae
mlhuynh <greenlinh@gmail.com>
2023-06-19 00:07:57 -0700

    Imported and implemented useState to handle rendering single chat entry in App component.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
 import ChatEntry from './components/ChatEntry';
+import { useState } from 'react';
 
 const App = () => {
+  const [chatData, setChatData] = useState(chatMessages)
+
   return (
     <div id="App">
       <header>
         <h1>Ada Chat Log</h1>
       </header>
       <main>
-        <ChatEntry />
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatEntry>
+          entries={chatData}
+        </ChatEntry>
+        {/* Wave 02: Render ChatLog component */}
       </main>
     </div>
   );
