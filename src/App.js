20606e86be40c5bc20d61c7ccb951d8c1f27d4cd
mlhuynh <greenlinh@gmail.com>
2023-06-19 20:11:26 -0700

    Imported and implemented ChatLog in App component.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
-import ChatEntry from './components/ChatEntry';
-// import { useState } from 'react';
+import ChatLog from './components/ChatLog.js';
+import { useState } from 'react';
+
+// const entries = chatMessages;  
 
 const App = () => {
-//   const [chatData, setChatData] = useState(chatMessages)
+  const [entries, setEntries] = useState(chatMessages)
 
-  const messageTest = chatMessages[0]
-  
   return (
     <div id="App">
       <header>
         <h1>Ada Chat Log</h1>
       </header>
       <main>
-        <ChatEntry>
-          {/* entries={chatData} */}
-          id={messageTest.id}
-          sender={messageTest.sender}
-          body={messageTest.body}
-          timeStamp={messageTest.timeStamp}
-          liked={messageTest.liked}
-        </ChatEntry>
-        {/* Wave 02: Render ChatLog component */}
+        <ChatLog entries={entries} />
       </main>
     </div>
   );
